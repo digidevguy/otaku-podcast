2ec3cc22ceeb810955ee36d024673ab920abf384
Matthew <49506518+Permafun@users.noreply.github.com>
2023-02-28 14:16:29 -0600

    adding an image, some placeholder text and adjusting divs

--- src/pages/anime/index.tsx
+++ src/pages/anime/index.tsx
@@ -1,6 +1,7 @@
 import { Article } from '@/types';
 import { getArticles } from '@/utils';
-import { SimpleGrid, VStack } from '@chakra-ui/react';
+import Image from 'next/image';
+import { Box, Flex, VStack, Text } from '@chakra-ui/react';
 import { GetStaticProps, NextPage } from 'next';
 import ArticleContent from '@/components/articles/ArticleContent';
 
@@ -18,18 +19,37 @@ export const getStaticProps: GetStaticProps = async () => {
 const BlogListPage: NextPage<{ articles: Article[] }> = ({ articles }) => {
 	return (
 		<>
-			<VStack p={4} spacing={4} maxW='850px' mx='auto'>
-				{articles.map((article) => (
-					<ArticleContent
-						key={article.id}
-						id={article.id}
-						title={article.attributes.title}
-						content={article.attributes.content}
-						image={article.attributes.image.data.attributes.url}
-						createdAt={article.attributes.createdAt}
+			<Flex flexDir='column'>
+				<Box pos='relative' overflow='hidden' h={['250px', '350px']} w='full'>
+					<Image
+						src='/gracia-dharma-qTlbO6mkQH0-unsplash.jpg'
+						alt='Anime pics'
+						fill
+						style={{ objectFit: 'cover' }}
 					/>
-				))}
-			</VStack>
+				</Box>
+				<Text p={2}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in
+					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
+				</Text>
+				<VStack p={4} spacing={4} maxW='850px' mx='auto'>
+					{articles.map((article) => (
+						<ArticleContent
+							key={article.id}
+							id={article.id}
+							title={article.attributes.title}
+							content={article.attributes.content}
+							image={article.attributes.image.data.attributes.url}
+							createdAt={article.attributes.createdAt}
+						/>
+					))}
+				</VStack>
+			</Flex>
 		</>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 import Head from 'next/head';
 
 export default function Home() {
@@ -12,6 +12,15 @@ export default function Home() {
 			</Head>
 			<Flex flexDir='column' gap={4} p={4}>
 				<Heading>Elevated x Otaku</Heading>
+				<Text maxW='50%' alignSelf='center'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in
+					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
+				</Text>
 			</Flex>
 		</>
 	);
