641713404fc3b4ea8e34c2e39c50d448f944b0a4
Matthew <49506518+Permafun@users.noreply.github.com>
2023-05-30 21:29:00 -0500

    Adjusted color-choice for approval

--- src/pages/color-choice.tsx
+++ src/pages/color-choice.tsx
@@ -1,23 +1,22 @@
 import { NextPage } from 'next';
 import { Flex, Heading, Stack } from '@chakra-ui/react';
-import Image from 'next/image';
-import cloudImg from '../../public/assets/shapes/cloud-banner.png';
 
 const ColorChoicePage: NextPage = () => {
 	return (
 		<Stack
 			direction='column'
-			minH='85vh'
 			p={4}
-			spacing={3}
-			justifyContent='center'
+			spacing={4}
 			alignItems='center'
 			textAlign='center'
-			minW='100%'
 		>
-			<Heading alignSelf='center'>Color Pallete Idea</Heading>
-			<Stack>
-				<Image src={cloudImg} alt='' />
+			<Heading alignSelf='center'>Proposed Color branding</Heading>
+			<Stack direction='row' w='full' justify='space-around'>
+				{[...Array(3)].map((_, i) => (
+					<Heading alignSelf='center' key={i}>
+						Brand.{i + 1}00
+					</Heading>
+				))}
 			</Stack>
 			<Stack direction='row' h='50vh' w='full' justify='space-evenly'>
 				<Flex bg='brand.100' w='25%' rounded={10} boxShadow='md' />
