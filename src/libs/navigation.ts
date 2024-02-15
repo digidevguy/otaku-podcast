69321b1192935e5991267b29dab6641d0c5a9049
Matthew <49506518+Permafun@users.noreply.github.com>
2024-02-15 14:34:32 -0600

    Removing /anime until CMS issue is addressed.

--- src/libs/navigation.ts
+++ src/libs/navigation.ts
@@ -1,7 +1,7 @@
 export const navigation = [
 	{ label: 'About', href: '/about' },
 	{ label: 'Podcast', href: '/podcast' },
-	{ label: 'Anime', href: '/anime' },
+	// { label: 'Anime', href: '/anime' },
 	{ label: 'Cosplay', href: '/cosplay' },
 	{ label: 'Contact', href: '/contact' },
 ];
